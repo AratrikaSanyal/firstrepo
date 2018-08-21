67a1504de00107a2b6510bf63b6960680b543d59
AratrikaSanyal <aratrika.sanyal@absolutdata.com>
2018-08-21 16:36:53 +0530

    Firstcommit

---
+++ Demo/StringDemo.java
@@ -0,0 +1,12 @@
+
+public class StringDemo {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String palindrome = "Malayalam";
+		int len= palindrome.length();
+		System.out.println("String length is ; " + len);
+	}
+
+}
---
+++ Temporary/src/com/adt/login/Checkstringlen.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.adt.login;
+
+/**
+ * @author aratrika
+ *
+ */
+public class Checkstringlen {
+
+	/**
+	 * 
+	 */
+	public Checkstringlen() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
